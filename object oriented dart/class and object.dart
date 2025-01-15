c7ff9faf762d834098d76dfc01ab01068afc505d
Bishal <173034130+Bishal-sub@users.noreply.github.com>
2025-01-15 11:27:20 +0545

    Add files via upload

---
+++ object oriented dart/class and object.dart
@@ -0,0 +1,25 @@
+class Car {
+  String? name;
+  int? year;
+  int? mileage;
+
+  // Method to display car details
+  void showDetail() {
+    print("Car Name is $name");
+    print("Car mileage is $mileage");
+    print("Car was made in $year");
+  }
+}
+
+void main() {
+  // Creating an object of the Car class using the default constructor
+  final carDetail = Car();
+
+  // Setting the fields of the Car object
+  carDetail.name = "Kia";
+  carDetail.mileage = 55;
+  carDetail.year = 2252;
+
+  // Displaying the details of the Car object
+  carDetail.showDetail();
+}
