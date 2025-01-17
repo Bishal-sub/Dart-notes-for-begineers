354094edfd1940a2b539b5782484603675011ad1
Bishal <173034130+Bishal-sub@users.noreply.github.com>
2025-01-17 08:48:23 +0545

    Update expression in function.dart

--- expression in function.dart
+++ expression in function.dart
@@ -13,20 +13,4 @@ int sum(List<int> numbers) => numbers.reduce((a, b) => a + b);
 // Function that uses an expression to find the maximum number
 int max(int a, int b) => a > b ? a : b;
 
-void main() {
-  // Using the square function
-  print(square(5)); // Output: 25
 
-  // Using the isEven function
-  print(isEven(10)); // Output: true
-
-  // Using the greet function
-  print(greet("Ram")); // Output: Hello, Ram!
-
-  // Using the sum function
-  var numbers = [1, 2, 3, 4, 5];
-  print(sum(numbers)); // Output: 15
-
-  // Using the max function
-  print(max(10, 20)); // Output: 20
-}
